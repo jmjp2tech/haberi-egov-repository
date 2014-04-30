6cbb198b7d9e75a684c8fe00f1f941549ca05fc3
JeanPierre <jmjp2tech@gmail.com>
2014-04-30 02:29:39 -0400

    payment type enum

--- egovEjbClient/ejbModule/com/haberi/egov/ejb/enums/PaymentTypeEnum.java
+++ egovEjbClient/ejbModule/com/haberi/egov/ejb/enums/PaymentTypeEnum.java
@@ -1,5 +1,7 @@
 package com.haberi.egov.ejb.enums;
 
+import org.apache.commons.lang3.StringUtils;
+
 public enum PaymentTypeEnum {
 	CREDIT_CARD("credit card"),MOBILE_MONEY("mobile_money"),TIGO_CASH("tigo_cash"),BANK_TRANSFER("bank_transfer"),COUNTER("counter"); 
 	
@@ -17,9 +19,16 @@ public enum PaymentTypeEnum {
 		this.code = code;
 	}
 	
-//	public static  getEnum(String paymentTypeStr){
-//		
-//	}
-//	
+	public static PaymentTypeEnum getEnum(String paymentTypeStr){
+		PaymentTypeEnum paymentTypeEnum = null ; 
+		
+		for(PaymentTypeEnum currentEnum : PaymentTypeEnum.values()){
+			if(StringUtils.equalsIgnoreCase(currentEnum.getCode(), paymentTypeStr)){
+				paymentTypeEnum = currentEnum ; 
+			}
+		}
+		return paymentTypeEnum;
+	}
+	
 
 }
